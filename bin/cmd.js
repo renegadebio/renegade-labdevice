457b0681a0bc498820d6938f0128d3d98d72b0b5
Marc Juul <github@juul.io>
2016-06-23 11:47:12 -0700

    preliminary support for server connection

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var buffersEqual = require('buffer-equal-constant-time');
 var ssh2 = require('ssh2');
 var minimist = require('minimist');
 
@@ -28,26 +27,39 @@ settings.device = argv.device || settings.device;
 settings.pubkey = argv.pubkey || settings.pubkey;
 settings.cmd = argv.cmd || settings.cmd;
 
-var pubKey = ssh2.utils.genPublicKey(ssh2.utils.parseKey(fs.readFileSync(settings.pubkey)));
 
-
-var conn = new Client();
+var conn = new ssh2.Client();
 
 conn.on('ready', function() {
     console.log("Connected!");
+    conn.exec('getLabel', function(err, stream) {
+        if(err) return console.error(err);
+        stream.on('data', function(data) {
+            console.log("got:", data);
+            stream.write("Thanks!");
+        });
+    });
 });
  
+
+conn.on('error', function(err) {
+    console.error("Connection error:", err);
+});
 
 conn.connect({
     host: settings.hostname,
     port: settings.port,
     username: settings.username,
-    privkey: fs.readFileSync(settings.privkey),
+    privateKey: fs.readFileSync(settings.privkey),
     hostHash: 'sha1',
     hostVerifier: function(hashedKey) {
         if(hashedKey === settings.hosthash) {
             return true;
         }
+        console.log("Untrusted host key!");
+        console.log("If you want to trust this host, set settings.hosthash to:");
+        console.log("  "+hashedKey);
+        console.log("");
         return false;
     }
 });
